992b5004ef2bd8451137fc045d59a039ff1495ad
Yulia Marushchenko <juliam7777@mail.ru>
2015-04-17 21:13:50 +0300

    Delete MathOperations.java

--- src/main/java/hometask/module02/calculator/MathOperations.java
+++
@@ -1,9 +0,0 @@
-package hometask.module02.calculator;
-
-/**
- * Created by Юля on 10.04.2015.
- */
-public interface MathOperations {
-
-    Value calculation (Value val1, Value val2) throws Exception;
-}
