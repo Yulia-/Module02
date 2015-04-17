84f4324b00c3f3ded70e17f9d8135ca0edab2409
Yulia Marushchenko <juliam7777@mail.ru>
2015-04-17 21:13:02 +0300

    Delete CalculationStrategy.java

--- src/main/java/hometask/module02/calculator/CalculationStrategy.java
+++
@@ -1,9 +0,0 @@
-package hometask.module02.calculator;
-
-/**
- * Created by Юля on 30.03.2015.
- */
-public interface CalculationStrategy {
-
-    String calculate(String expression) throws Exception;
-}
